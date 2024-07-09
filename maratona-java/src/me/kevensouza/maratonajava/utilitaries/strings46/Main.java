640cdf3fe75b55ad86d8c52419ccfc81d9bcd003
Keven Souza <souzadossantoskeven@gmail.com>
2024-07-09 20:00:16 -0300

    strings

---
+++ maratona-java/src/me/kevensouza/maratonajava/utilitaries/strings46/Main.java
@@ -0,0 +1,41 @@
+package me.kevensouza.maratonajava.utilitaries.strings46;
+
+public class Main {
+    public static void main(String[] args) {
+        long init = System.currentTimeMillis();
+        concatStrings(50_000);
+        long end = System.currentTimeMillis();
+        System.out.println((end - init) + "ms");
+
+        init = System.currentTimeMillis();
+        concatStringsWithBuilder(100_000);
+        end = System.currentTimeMillis();
+        System.out.println((end - init) + "ms");
+
+        init = System.currentTimeMillis();
+        concatStringsWithBuffer(100_000);
+        end = System.currentTimeMillis();
+        System.out.println((end - init) + "ms");
+    }
+
+    public static void concatStrings(int length) {
+        String text = "";
+        for (int i = 0; i < length; i++) {
+            text += i;
+        }
+    }
+
+    public static void concatStringsWithBuilder(int length) {
+        StringBuilder text = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            text.append(i);
+        }
+    }
+
+    public static void concatStringsWithBuffer(int length) {
+        StringBuffer text = new StringBuffer(length);
+        for (int i = 0; i < length; i++) {
+            text.append(i);
+        }
+    }
+}
