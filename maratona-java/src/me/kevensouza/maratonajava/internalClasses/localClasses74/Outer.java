33de3828f5051c4d7edec92d54ae2da4bcb78c5f
Keven Souza <souzadossantoskeven@gmail.com>
2024-08-07 18:30:43 -0300

    Refactor local class

--- maratona-java/src/me/kevensouza/maratonajava/internalClasses/localClasses74/Outer.java
+++ maratona-java/src/me/kevensouza/maratonajava/internalClasses/localClasses74/Outer.java
@@ -1,15 +1,21 @@
 package me.kevensouza.maratonajava.internalClasses.localClasses74;
 
 public class Outer {
-    private String nome = "Keven";
+    private String name = "Keven";
 
-    private static void localClass() {
+    public void localClass() {
+        String lastName = "Souza";
 
         class Inner {
-
+            void print() {
+                System.out.println("\"sfd man!\"\n- ass: " + name + " " + lastName);
+            }
         }
+
+        new Inner().print();
     }
 
     public static void main(String[] args) {
+        new Outer().localClass();
     }
 }
